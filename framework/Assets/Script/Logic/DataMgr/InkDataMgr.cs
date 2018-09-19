75f51593855480d500321eb9d7c6256be3334038
603010999 <603010999@qq.com>
2018-09-19 22:19:46 +0800

    add

--- framework/Assets/Script/Logic/DataMgr/InkDataMgr.cs
+++ framework/Assets/Script/Logic/DataMgr/InkDataMgr.cs
@@ -11,23 +11,27 @@ public class InkStoryData
     //
 }
 
-public class InkDataMgr : Singleton<InkDataMgr> 
+public class InkDataMgr : Singleton<InkDataMgr>
 {
     //当前运行故事
     private Story m_curStory;
     
+    //当前内容 固定2个，0:名字 1:文本内容
+    private List<string> m_curContent = new List<string>();
+    
+
     //设置当前故事
-    public void SetCurStory(string playerId,string storyId)
-    {        
+    public void SetCurStory(string playerId, string storyId)
+    {
         var text = InkConfigMgr.Instance.GetInkText(playerId, storyId);
 
         if (string.IsNullOrEmpty(text))
         {
             return;
         }
-        
-        
-        m_curStory = new Story(text);  
+
+
+        m_curStory = new Story(text);
     }
 
     //能否继续
@@ -40,7 +44,7 @@ public class InkDataMgr : Singleton<InkDataMgr>
 
         return m_curStory.canContinue;
     }
-    
+
     //继续进行
     public string ContinueStory()
     {
@@ -50,10 +54,10 @@ public class InkDataMgr : Singleton<InkDataMgr>
         }
 
         //todo 记录数据
-        
+
         return m_curStory.Continue();
     }
- 
+
     //获取选择列表
     public List<Choice> GetChoiceList()
     {
@@ -66,10 +70,10 @@ public class InkDataMgr : Singleton<InkDataMgr>
         {
             return null;
         }
-        
+
         return m_curStory.currentChoices;
-    }    
-    
+    }
+
     //选择选项
     public void Choose(int index)
     {
@@ -77,11 +81,48 @@ public class InkDataMgr : Singleton<InkDataMgr>
         {
             return;
         }
-        
+
+        var list = GetChoiceList();
+        if (list == null)
+        {
+            return;
+        }
+
         m_curStory.ChooseChoiceIndex(index);
-        
+
         //记录内容  做选择的一定是主角
-        
+
+
         //todo 发送界面刷新消息
+    }
+
+    public List<string> ParseContent(string content)
+    {
+        m_curContent.Clear();
+        
+        if (string.IsNullOrEmpty(content))
+        {
+            return m_curContent;
+        }
+        
+        var index = content.IndexOf(":");
+
+        if (index == -1)
+        {
+            //中文也匹配下
+            index = content.IndexOf("：");
+        }
+
+        if (index == -1)
+        {
+            var loginfo = string.Format("not find : in content: {0} ", content);
+            Debug.Log(loginfo);
+            return m_curContent;
+        }
+
+        m_curContent.Add(content.Substring(0, index));
+        m_curContent.Add(content.Substring(index));
+
+        return m_curContent;
     }
 }
